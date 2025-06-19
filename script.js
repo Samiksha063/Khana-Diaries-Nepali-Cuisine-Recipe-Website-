31eec85bf0c45b2f90c775e5fb8600ad3d342577
Samiksha063 <samiksha3stha@gmail.com>
2025-06-19 22:16:35 +0545

    added search recipe feature

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
+// -------------------search recipe feature--------------------
+window.addEventListener("DOMContentLoaded", () => {
+
+const search = document.getElementById("search");
+const message = document.getElementById("message");
+const clearSearch  = document.getElementById("clearSearch");
+
+// array of recipes
+const recipes= ["dalbhat", "dhindo", "momo" ,"wo", "chhoila", "selroti", "yomari", "tama", "milktea","lassi","bhatmassadheko", "saag", "chokha", "gundruksaadheko", "jerry","kheer", "chickencurry", "thwon", "chatamari", "tomatopickle", "pirooaloo", "momokoachar"];
+
+search.addEventListener("keypress", (event) => {
+    if(event.key === "Enter"){
+        const query = event.target.value.toLowerCase().replace(/\s+/g, ''); //replace(/\s+/g, '')=> regular expression removes white space globally
+
+        if(recipes.includes(query)){
+            message.textContent = "";
+            window.location.href = `../pages/allRecipes.html?recipe=${encodeURIComponent(query)}`;
+        }else{
+            message.textContent = "Recipe not found";
+        }
+
+    }
+});
+
+clearSearch.addEventListener("click", () => {
+    search.value = "";
+    message.textContent = ""; //clears previous message
+});
+
+const recipeParams = new URLSearchParams(window.location.search);
+const recipe = recipeParams.get("recipe");
+if(recipe){
+    const recipeCard = document.getElementById(recipe);
+
+    if(recipeCard){
+        recipeCard.scrollIntoView({
+            behavior : "smooth",
+            block : "center"
+        });
+
+        recipeCard.classList.add("highlight");
+
+        setTimeout(() => {
+            recipeCard.classList.remove("highlight");
+        },3000); //removes highlight after 3sec
+    }else{
+        document.querySelectorAll(".allRecipeCard").forEach(recipeCard => {
+            recipeCard.style.display = "none";
+        });
+
+        
+            message.textContent = "Recipe not found";
+        
+    }
+}
+
+
+});
+//write the message "recipe not found when invalid recipe is written from any pages"
+
+
+
+
+
+
+
+
+
 
 // ------------All Recipe page category filter------------------------
 
