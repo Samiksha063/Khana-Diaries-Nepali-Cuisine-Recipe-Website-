091ff2ec20607dad787739e72d859abbda82ba5a
Samiksha063 <samiksha3stha@gmail.com>
2025-07-11 22:30:13 +0545

    added image slider in home page

--- script/main.js
+++ script/main.js
@@ -22,3 +22,33 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+//home page image slider
+$(document).ready(function () {
+    let index = 0;
+    const slideCount = $(".slide").length;
+    let dots = $(".dot");
+
+    function showSlide(){ //shows current slide and highlight the active class dot
+        $(".slider-track").css("transform", `translateX(-${index * 100}%)`);
+
+        dots.removeClass("active-dot");
+
+        dots.eq(index).addClass("active-dot");
+
+    }
+
+    function slideNext() {
+      index = (index + 1) % slideCount;
+      showSlide();
+        
+    }
+
+    setInterval(slideNext, 4000);
+
+    dots.click(function(){
+        index = $(this).data("index"); // Get the index number from the clicked dot
+        showSlide();
+
+    });
+  });
